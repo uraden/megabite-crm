c50e22c0f23725355f0a332a52d8b94e169a7e7d
uraden <jfahriddinov97@gmail.com>
2025-01-17 21:34:22 +0500

    new chagne

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,55 @@
-function Main() {
-  return (
-    <div>Main</div>
-  )
+import { useState, useEffect } from "react";
+import { getAllProducts } from "../Products/request";
+
+interface ProductsProps {
+  id: string;
+  category: string;
+  description: string;
+  name: string;
+  url: string;
+  image: string;
 }
 
-export default Main
+function Main() {
+  const [products, setProducts] = useState<ProductsProps[]>([]);
+
+  const fetchProducts = async () => {
+    const products = await getAllProducts();
+    if (products) {
+      setProducts(products);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+  return (
+    <div>
+      <h1>Продукты</h1>
+      <div className="flex flex-wrap gap-4 mt-4">
+        {products.map((product) => (
+          <div>
+            <div key={product.id} className="w-[250px] p-4 border">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-[150px] h-[100px] m-auto"
+              />
+              <div className="mt-6">
+                <h2>{product.name}</h2>
+                <p>{product.description}</p>
+              </div>
+            </div>
+            <div className="border-x border-b p-2 flex justify-center hover:cursor-pointer" onClick={() => console.log('Добавлено в корзину', product.name)}>
+              В корзину
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Main;
